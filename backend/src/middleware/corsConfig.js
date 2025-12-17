81927fef4e8916fcb80cbcb5a7eb2136d9de85b6
Sebaxis07 <thefilex07@gmail.com>
2025-12-17 05:14:54 -0300

    Arreglo minimo

--- backend/src/middleware/corsConfig.js
+++ backend/src/middleware/corsConfig.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://star-white.vercel.app',
+  'https://star-white-raaub5rjy-sebastaanas-projects.vercel.app',
   process.env.FRONTEND_URL
 ].filter(Boolean); // Eliminar valores undefined
 
